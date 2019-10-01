d9f92c8baa80c05df76bd74915aa561171b8e3ec
Matheus Lima <matheusldasilva20088@gmail.com>
2019-10-01 12:17:35 -0300

    conexao mongodb e sessão usuario

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
 
 const app = express();
+mongoose.connect('mongodb+srv://matheus150797:matheus150797@orministack-k2833.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 //req.query = acessar parametros na url
 // req.params = acessar route params (edição e delete)
 
-app.put('/users/:id', (req, res) => {
-   return res.json({id: req.params.id})  
-});
+//req.body = aceessar corpo
+app.use(express.json());
+app.use(routes);
+
+
+
 
 app.listen(8080);
